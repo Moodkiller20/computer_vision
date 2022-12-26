90c14d2cb8324f252c1b65c477198cf973c660f4
Giovanni Tshiabngu <61130107+Moodkiller20@users.noreply.github.com>
2022-12-25 22:38:03 -0500

    vide capture with openCv2

---
+++ videoCapture.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0) # To connect to  your camera.
+
+# un comment this line if you have a video file.
+# cap = cv2.VideoCapture("PATH TO YOUR VIDEO FILE")
+
+# This will go until with press a key to quit the program.
+while True:
+    """
+    *   ret is a boolean variable that returns true if the frame is available. So let say if You only have one camera 
+        and that camera is already being used by anther software, ret will return False
+        
+    *   frame is an image array vector captured based on the default frames per second defined explicitly or implicitly
+    """
+    ret, frame = cap.read()
+
+    # imshow takes in the frame title and the frame value return by the above line and display
+    cv2.imshow('Frame Title', frame)
+
+    # Wait for 10 millisecond and if in that x millisecond you press 'q' it will break out of the loop.
+    if cv2.waitKey(10) == ord('q'):
+        break
+
+# This release the camera resources so a another program can use it.
+cap.release()
+
+cv2.destroyAllWindows()
